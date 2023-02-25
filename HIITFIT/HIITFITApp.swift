b375ccb002c8905b762d86df96f518c822b748e7
Azizbek Asadov <azizbek1998@icloud.com>
2023-02-25 17:08:32 +0500

    Initial Commit

---
+++ HIITFIT/HIITFITApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITFITApp.swift
+//  HIITFIT
+//
+//  Created by Azizbek Asadov on 25/02/23.
+//
+
+import SwiftUI
+
+@main
+struct HIITFITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
